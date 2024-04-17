5d9fbc63c8467cd883bf3d0f4dccc8f89f8e949c
Persiano99 <sorenrahimi99@gmail.com>
2024-04-18 00:34:30 +0200

    Initial commit

---
+++ src/main/java/sorenrahimi/g3s3m2/G3S3M2Application.java
@@ -0,0 +1,13 @@
+package sorenrahimi.g3s3m2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class G3S3M2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(G3S3M2Application.class, args);
+	}
+
+}
---
+++ src/test/java/sorenrahimi/g3s3m2/G3S3M2ApplicationTests.java
@@ -0,0 +1,13 @@
+package sorenrahimi.g3s3m2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class G3S3M2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
