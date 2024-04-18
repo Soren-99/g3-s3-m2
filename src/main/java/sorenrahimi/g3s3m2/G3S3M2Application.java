60a917eb123e827ba6492a1e7b0b9389e79c9802
Persiano99 <sorenrahimi99@gmail.com>
2024-04-18 17:34:49 +0200

    esercizio 1

--- src/main/java/sorenrahimi/g3s3m2/G3S3M2Application.java
+++ src/main/java/sorenrahimi/g3s3m2/G3S3M2Application.java
@@ -6,9 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class G3S3M2Application {
 
-	public static void main(String[] args) {
-
-		SpringApplication.run(G3S3M2Application.class, args);
+	public static void main(String[] args) {SpringApplication.run(G3S3M2Application.class, args);
 	}
 
 }
